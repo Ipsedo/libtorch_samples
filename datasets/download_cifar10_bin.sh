ab4af41a9fbef6b42bd1f80e0b3ddda8a626acc8
Ipsedo <berriens@gmail.com>
2019-06-01 21:42:32 +0200

    add download cifar-10 script

---
+++ datasets/download_cifar10_bin.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Assume you execute this script in /path/to/libtorch_samples/datasets
+if ! [[ -d "./donwloaded" ]]; then
+    mkdir downloaded
+fi
+cd downloaded
+
+if ! [[ -d "cifar-10-batches-bin/" ]]; then
+	wget https://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz
+	tar xvzf cifar-10-binary.tar.gz
+	rm -f cifar-10-binary.tar.gz
+fi
+
+cd ..
