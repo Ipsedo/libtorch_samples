6b4f41720fb17e0a4fc729be798af5e921f5cb3f
Ipsedo <berriens@gmail.com>
2019-06-04 19:42:47 +0200

    fait CNN cifar -> shape à testé

--- src/models/conv_models.h
+++ src/models/conv_models.h
@@ -27,4 +27,56 @@ struct MNIST_ConvNet : torch::nn::Module {
     torch::nn::Linear l1{nullptr};
 };
 
+struct CIFAR_ConvNet : torch::nn::Module {
+    CIFAR_ConvNet() {
+        int c1_ft_m = 9;
+        int c2_ft_m = 32;
+
+        linear_dim = int(
+                pow((32.0 - (conv_kernel_sizes[0] - 1.0)) / 2.0 - (conv_kernel_sizes[1] - 1.0), 2.0)
+                * c2_ft_m);
+
+        c1 = register_module("c1",
+                torch::nn::Conv2d(torch::nn::Conv2dOptions(3, c1_ft_m,
+                        {conv_kernel_sizes[0], conv_kernel_sizes[0]})));
+        c2 = register_module("c2",
+                torch::nn::Conv2d(torch::nn::Conv2dOptions(c1_ft_m, c2_ft_m,
+                        {conv_kernel_sizes[1], conv_kernel_sizes[1]})));
+
+        l1 = register_module("l1", torch::nn::Linear(linear_dim, linear_dim * 2));
+
+        bn = register_module("bn",
+                torch::nn::BatchNorm(torch::nn::BatchNormOptions(linear_dim * 2)));
+
+        l2 = register_module("l2", torch::nn::Linear(linear_dim * 2, nb_class));
+    }
+
+    torch::Tensor forward(torch::Tensor x) {
+        x = torch::relu(torch::max_pool2d(c1->forward(x),
+                {maxpool_size[0], maxpool_size[0]},
+                {maxpool_size[0], maxpool_size[0]}));
+
+        x = torch::relu(torch::max_pool2d(c2->forward(x),
+                {maxpool_size[1], maxpool_size[1]},
+                {maxpool_size[1], maxpool_size[1]}));
+
+        x = x.flatten(1, -1);
+
+        x = l1->forward(x);
+        x = bn->forward(x);
+        x = l2->forward(x);
+        x = torch::softmax(x, 1);
+
+        return x;
+    }
+
+    torch::nn::Conv2d c1{nullptr}, c2{nullptr};
+    torch::nn::Linear l1{nullptr}, l2{nullptr};
+    torch::nn::BatchNorm bn{nullptr};
+
+    int linear_dim, nb_class = 10;
+    int conv_kernel_sizes[2]{5, 3};
+    int maxpool_size[2]{3, 2};
+};
+
 #endif //LIBTORCH_SAMPLES_CONV_MODELS_H
