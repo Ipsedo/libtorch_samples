77d5615a41695f1a5d6d39baf446802eacabd2c5
Ipsedo <berriens@gmail.com>
2019-06-09 05:08:26 +0200

    updated build scripts

--- build_cu90.sh
+++ build_cu90.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-if [ -d "./build" ]; then
-	rm -rf ./build
+BUILD_DIR=build_cu90
+if [ -d "./${BUILD_DIR}" ]; then
+	rm -rf ./${BUILD_DIR}
 fi
-mkdir build
-cd build
+mkdir ${BUILD_DIR}
+cd ${BUILD_DIR}
 cmake -DCUDA_HOST_COMPILER=/usr/bin/gcc-5 -DCUDNN_INCLUDE_DIR=/usr/local/cuda-9.0/include -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-9.0 -DCUDNN_LIBRARY=/usr/local/cuda-9.0/lib64/libcudnn.so ..
 make
 cd ..
